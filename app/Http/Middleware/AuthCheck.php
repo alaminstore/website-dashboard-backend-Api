d9f84cfb774c6035a82ca02493ad710acc9f1adc
SM Alamin <alamin.piash.ap23@gmail.com>
2021-06-14 22:04:26 +0600

    New condition added in auth

--- app/Http/Middleware/AuthCheck.php
+++ app/Http/Middleware/AuthCheck.php
@@ -16,6 +16,9 @@ class AuthCheck
      */
     public function handle(Request $request, Closure $next)
     {
+        if(!session()->has('LoggedUser') && $request->path() =='/'){
+            return redirect('auth/login');
+        }
         if(!session()->has('LoggedUser') && ($request->path() !='auth/login' && $request->path() !='auth/register' )){
             return redirect('auth/login')->with('unauthorized','You must be logged in then go');
         }
@@ -23,8 +26,13 @@ class AuthCheck
         if(session()->has('LoggedUser') && ($request->path() == 'auth/login' || $request->path() == 'auth/register' ) ){
             return back();
         }
+
+
+
+
         return $next($request)->header('Cache-Control','no-cache, no-store, max-age=0, must-revalidate')
                               ->header('Pragma','no-cache')
                               ->header('Expires','Sat 01 Jan 1990 00:00:00 GMT');
+
     }
 }
